44b4d87c864023df76501d74ba6306f5ddeede02
Josh Nichols <josh@technicalpickles.com>
2008-08-25 17:21:40 -0400

    Going back to the basics, so I don't embarass myself further. Will take a look at doing spec/factories when I actually have time to test.

--- init.rb
+++ init.rb
@@ -1,6 +1,4 @@
 require 'factory_girl'
-
-Dir[File.join(RAILS_ROOT, 'test', 'factories' '*.rb')].each { |file| require file }
-Dir[File.join(RAILS_ROOT, 'test' 'factories.rb')].each { |file| require file }
-Dir[File.join(RAILS_ROOT, 'spec' 'factories' '*.rb')].each { |file| require file }
-Dir[File.join(RAILS_ROOT, 'spec' 'factories.rb')].each { |file| require file }
+Dir[File.join(RAILS_ROOT, 'test', 'factories', '*.rb')].each do |file|
+  require file
+end
