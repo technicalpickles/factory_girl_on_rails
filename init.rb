1a9a08eaa4bfa15b420d3d7b33ca13bbf4cd12a4
Josh Nichols <josh@technicalpickles.com>
2008-08-25 14:52:32 -0400

    Forgot closing parens.

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require 'factory_girl'
 
-Dir[File.join(RAILS_ROOT, *%(test factories *.rb)].each { |file| require file }
-Dir[File.join(RAILS_ROOT, *%(test factories.rb)].each { |file| require file }
-Dir[File.join(RAILS_ROOT, *%(spec factories *.rb)].each { |file| require file }
-Dir[File.join(RAILS_ROOT, *%(spec factories.rb)].each { |file| require file }
+Dir[File.join(RAILS_ROOT, *%(test factories *.rb))].each { |file| require file }
+Dir[File.join(RAILS_ROOT, *%(test factories.rb))].each { |file| require file }
+Dir[File.join(RAILS_ROOT, *%(spec factories *.rb))].each { |file| require file }
+Dir[File.join(RAILS_ROOT, *%(spec factories.rb))].each { |file| require file }
