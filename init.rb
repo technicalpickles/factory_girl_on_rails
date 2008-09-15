95de23aec4f2d3cce02f731339af790b637e3a10
Josh Nichols <josh@technicalpickles.com>
2008-09-14 23:05:33 -0400

    Use config.gem to depend on factory_girl.

--- init.rb
+++ init.rb
@@ -1,9 +1,11 @@
-require 'factory_girl'
+config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
 
-%w(test spec).each do |dir|
-  factories = File.join(RAILS_ROOT, dir, 'factories.rb')
-  require factories if File.exists?(factories)
-  Dir[File.join(RAILS_ROOT, dir, 'factories', '*.rb')].each do |file|
-    require file
-  end
+config.after_initialize do
+  %w(test spec).each do |dir|
+    factories = File.join(RAILS_ROOT, dir, 'factories.rb')
+    require factories if File.exists?(factories)
+    Dir[File.join(RAILS_ROOT, dir, 'factories', '*.rb')].each do |file|
+      require file
+    end
+  end  
 end
